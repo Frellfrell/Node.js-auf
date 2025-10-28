f34bd3592b095a6c51410afe685bcffb68f3ebd0
Frellfrell <ella.gubareva.24@gmail.com>
2025-10-28 12:05:38 +0100

    Update task_1.js

--- Auf3_movecopydel/Task_1/task_1.js
+++ Auf3_movecopydel/Task_1/task_1.js
@@ -1,1 +1,11 @@
 const fs = require('fs');
+
+
+fs.mkdir('myFolder', (err) => {
+    if (err) {
+        console.error('Error creating directory:', err);
+    } else {
+        console.log('Directory created successfully!')
+    
+    
+});
