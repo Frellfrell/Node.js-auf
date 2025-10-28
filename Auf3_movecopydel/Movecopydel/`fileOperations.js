2144e088c1b9ef4c162ff2af68870ad0e17be703
Frellfrell <ella.gubareva.24@gmail.com>
2025-10-28 11:48:19 +0100

    Update `fileOperations.js

--- Auf3_movecopydel/Movecopydel/`fileOperations.js
+++ Auf3_movecopydel/Movecopydel/`fileOperations.js
@@ -1,4 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 
+// Определяем пути к файлам
+const sourceFilePath = path.join(__dirname, 'example.jpg'); // исходный файл
+const renamedFilePath = path.join(__dirname, 'renamedExample.jpg'); // после переименования
+const copyFilePath = path.join(__dirname, 'copyOfExample.jpg'); // после копирования
 
+
+//  Проверим, существует ли исходный файл
+if (!fs.existsSync(sourceFilePath)) {
+  console.error('Файл example.jpg не найден в текущей директории!');
+  process.exit(1);
+}
