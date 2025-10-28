c337221b0664164bbd35357532183862f72a5286
Frellfrell <ella.gubareva.24@gmail.com>
2025-10-28 11:51:13 +0100

    Update `fileOperations.js

--- Auf3_movecopydel/Movecopydel/`fileOperations.js
+++ Auf3_movecopydel/Movecopydel/`fileOperations.js
@@ -12,3 +12,27 @@ if (!fs.existsSync(sourceFilePath)) {
   console.error('Файл example.jpg не найден в текущей директории!');
   process.exit(1);
 }
+
+// Переименование файла
+fs.rename(sourceFilePath, renamedFilePath, (err) => {
+  if (err) {
+    return console.error('Ошибка при переименовании файла:', err);
+  }
+  console.log('Файл успешно переименован в renamedExample.jpg');
+
+  // Копирование файла
+  fs.copyFile(renamedFilePath, copyFilePath, (err) => {
+    if (err) {
+      return console.error('Ошибка при копировании файла:', err);
+    }
+    console.log('Файл успешно скопирован в copyOfExample.jpg');
+
+    // Удаление оригинального файла
+    fs.unlink(renamedFilePath, (err) => {
+      if (err) {
+        return console.error('Ошибка при удалении файла:', err);
+      }
+      console.log('Файл renamedExample.jpg успешно удален.');
+    });
+    });
+});
