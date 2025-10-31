dd1b4045569f2885f002062fcb7a8cd9ecd04dd3
Frellfrell <ella.gubareva.24@gmail.com>
2025-10-31 17:09:54 +0100

    Update date-format.js

--- Auf4_timedateformat/date-format.js
+++ Auf4_timedateformat/date-format.js
@@ -5,6 +5,6 @@ const now = moment();
 console.log("Обычный формат:", now.format());
 
 
-console.log("Читаемый формат (YYYY-MM-DD HH:mm:ss):", now.format('YYYY-MM-DD HH:mm:ss'));
-console.log("Читаемый формат (DD/MM/YYYY):", now.format('DD/MM/YYYY'));// День-месяц-год
+console.log("Читаемый формат (YYYY-MM-DD HH:mm:ss):", now.format('YYYY-MM-DD HH:mm:ss'));// День-месяц-год
+console.log("Читаемый формат (DD/MM/YYYY):", now.format('DD/MM/YYYY'));// День/месяц/год
 console.log("Читаемый формат (MMMM Do YYYY, h:mm:ss a):", now.format('MMMM Do YYYY, h:mm:ss a')); // Месяц , день с суффиксом, год
