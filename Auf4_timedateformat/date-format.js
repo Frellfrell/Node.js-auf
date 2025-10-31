2b9985118a45cd81aa722d9d9cfeaae00ff7202b
Frellfrell <ella.gubareva.24@gmail.com>
2025-10-31 17:02:33 +0100

    Update date-format.js

---
+++ Auf4_timedateformat/date-format.js
@@ -0,0 +1,10 @@
+const moment = require('moment');
+
+const now = moment();
+
+console.log("Обычный формат:", now.format());
+
+
+console.log("Читаемый формат (YYYY-MM-DD HH:mm:ss):", now.format('YYYY-MM-DD HH:mm:ss'));
+console.log("Читаемый формат (DD/MM/YYYY):", now.format('DD/MM/YYYY'));
+console.log("Читаемый формат (MMMM Do YYYY, h:mm:ss a):", now.format('MMMM Do YYYY, h:mm:ss a'));
