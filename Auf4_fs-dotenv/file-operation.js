8d02d7f2a0630ed58eb146db89589eb2e8939eef
Frellfrell <ella.gubareva.24@gmail.com>
2025-11-03 09:20:22 +0100

    Update file-operation.js

---
+++ Auf4_fs-dotenv/file-operation.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
+const filePath = process.env.FILE_PATH;
+
+if (!filePath) {
+    console.error('FILE_PATH is not defined in the environment variables.');
+    process.exit(1);
+}
+
+const textToWrite = 'Hello, this is a test file created using dotenv configuration!';
+// Function to write to a file
+fs.writeFileSync(filePath, textToWrite, 'utf8'); 
+console.log(`File written successfully to ${filePath}`);
+
+// Function to read a file
+const data = fs.readFileSync(filePath, 'utf8');
+
+console.log(`\nFile read successfully from ${filePath}`);
