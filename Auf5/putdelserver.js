efa16e3e9a3ea524bc29215af1160553adebd19c
Frellfrell <ella.gubareva.24@gmail.com>
2025-11-07 15:57:21 +0100

    Update putdelserver.js

---
+++ Auf5/putdelserver.js
@@ -0,0 +1,22 @@
+
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.method === 'PUT') {
+    res.statusCode = 200;
+    res.end('PUT-запрос обработан');
+  } else if (req.method === 'DELETE') {
+    res.statusCode = 200;
+    res.end('DELETE-запрос обработан');
+  } else {
+    res.statusCode = 405; 
+    res.end('Метод не поддерживается');
+  }
+});
+server.listen(PORT, () => {
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
+});
