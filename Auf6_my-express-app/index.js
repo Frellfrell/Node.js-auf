9758fe421402133060291ba4491005d1b0363719
Frellfrell <ella.gubareva.24@gmail.com>
2025-11-07 15:16:37 +0100

    Update index.js

--- Auf6_my-express-app/index.js
+++ Auf6_my-express-app/index.js
@@ -1,11 +1,38 @@
 import express from "express";
 import dotenv from "dotenv";
 
+dotenv.config();
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello, Express with ES Modules!");
+    try {
+         res.status(200).send("Hello, World!");
+  } catch (error) {
+    console.error("Ошибка при обработке GET-запроса:", error);
+    res.status(500).send("внутренняя ошибка сервера");
+  }
+});
+
+app.post("/", (req, res) => {
+  try {
+    const data = req.body;
+
+    if (!data || Object.keys(data).length === 0) {
+      return res.status(400).json({ error: "Данные не были отправлены" });
+    }
+
+    res.status(200).json({
+      message: "Данные успешно получены!",
+      receivedData: data,
+    });
+  } catch (error) {
+    console.error("Ошибка при обработке POST-запроса:", error);
+    res.status(500).json({ error: "Произошла ошибка при выполнении POST-запроса" });
+  }
 });
 
 app.listen(PORT, () => {
