ea352dbf727f1974575172bde3992f72ae4d6736
Frellfrell <ella.gubareva.24@gmail.com>
2025-11-07 15:37:38 +0100

    Update authserver.js

--- Auf5/authserver.js
+++ Auf5/authserver.js
@@ -1,1 +1,25 @@
 const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  // Получаем заголовок Authorization
+  const authHeader = req.headers['authorization'];
+
+   res.setHeader('Content-Type', 'text/plain');
+
+  // Проверяем наличие заголовка Authorization
+  if (!authHeader) {
+    // Если нет — возвращаем 401 Unauthorized
+    res.statusCode = 401;
+    res.end('Unauthorized');
+  } else {
+    // Если есть — возвращаем 200 OK
+    res.statusCode = 200;
+    res.end('Authorization header received');
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
