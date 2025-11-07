d3405f1a7150516f92e02652950cd4ea40bfb616
Frellfrell <ella.gubareva.24@gmail.com>
2025-11-07 16:08:17 +0100

    Update authserver.js

--- Auf5/authserver.js
+++ Auf5/authserver.js
@@ -1,6 +1,7 @@
 const http = require('http');
+require('dotenv').config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   // Получаем заголовок Authorization
