2f8b2c8629cd97e041f89af648b413fe1d914410
Frellfrell <ella.gubareva.24@gmail.com>
2025-11-07 15:00:37 +0100

    Update index.js

--- Auf6_my-express-app/index.js
+++ Auf6_my-express-app/index.js
@@ -1,1 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
 
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, Express with ES Modules!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
