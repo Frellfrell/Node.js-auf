1df5a16966dd68604699700f732583eee6260c42
Frellfrell <ella.gubareva.24@gmail.com>
2025-11-11 15:50:39 +0100

    Update setup.js

--- Auf6_my-express-app/setup.js
+++ Auf6_my-express-app/setup.js
@@ -1,1 +1,9 @@
+import db from "./db.js";
 
+const createTableQuery = `
+CREATE TABLE IF NOT EXISTS products (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  price DECIMAL(10, 2) NOT NULL
+)
+`;
