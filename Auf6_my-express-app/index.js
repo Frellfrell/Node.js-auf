17a52d26495efa21534902d5ae3c312bbd48fe3e
Frellfrell <ella.gubareva.24@gmail.com>
2025-11-12 11:12:18 +0100

    Update index.js

--- Auf6_my-express-app/index.js
+++ Auf6_my-express-app/index.js
@@ -9,33 +9,38 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/products", (_req, res) => {
+app.get("/products", async (_req, res) => {
     try {
-         res.status(200).send("Hello, World!");
+      const [rows] = await db.promise().query("SELECT * FROM products");
+         res.status(200).json(rows);
   } catch (error) {
-    console.error("Ошибка при обработке GET-запроса:", error);
-    res.status(500).send("внутренняя ошибка сервера");
+    console.error("Ошибка при обработке GET/products:", error);
+    res.status(500).json("внутренняя ошибка сервера");
   }
 });
 
-app.post("/", (req, res) => {
+app.post("/products", async (req, res) => {
   try {
-    const data = req.body;
+    const {name, price} = req.body;
 
-    if (!data || Object.keys(data).length === 0) {
-      return res.status(400).json({ error: "Данные не были отправлены" });
+    if (!name || !price) {
+      return res.status(400).json({ error: "Name and price are required" });
     }
 
-    res.status(200).json({
+    const sql = "INSERT INTO products (name, price) VALUES (?, ?)";
+    const [result] = await db.promise().query(sql, [name, price]);
+
+    res.status(201).json({
       message: "Данные успешно получены!",
-      receivedData: data,
+      productId: result.insertId,
     });
   } catch (error) {
-    console.error("Ошибка при обработке POST-запроса:", error);
-    res.status(500).json({ error: "Произошла ошибка при выполнении POST-запроса" });
+    console.error("Ошибка при обработке POST/products:", error);
+    res.status(500).json({ error: "Произошла ошибка при выполнении POST/products" });
   }
 });
-app.use((req, res) => {
+// Обработка несуществующих маршрутов
+app.use((_req, res) => {
   res.status(404).json({ error: "Маршрут не найден" });
 });
 
