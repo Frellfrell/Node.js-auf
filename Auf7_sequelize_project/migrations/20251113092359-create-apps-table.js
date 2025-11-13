f7d30109fd87ca797d4704f1bac8d112c2ad6c7c
Frellfrell <ella.gubareva.24@gmail.com>
2025-11-13 10:28:13 +0100

    Update 20251113092359-create-apps-table.js

--- Auf7_sequelize_project/migrations/20251113092359-create-apps-table.js
+++ Auf7_sequelize_project/migrations/20251113092359-create-apps-table.js
@@ -22,11 +22,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('Apps');
   }
 };
