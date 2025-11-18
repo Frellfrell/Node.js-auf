54b5777b869e106b95a7b13811d9108785dc2918
Frellfrell <ella.gubareva.24@gmail.com>
2025-11-18 09:10:27 +0100

    Update 20251118080601-create-books-table.cjs

--- Auf8_book_project/migrations/20251118080601-create-books-table.cjs
+++ Auf8_book_project/migrations/20251118080601-create-books-table.cjs
@@ -3,20 +3,27 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+   await queryInterface.createTable("Books", {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    author: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    year: {
+      type: Sequelize.INTEGER
+    }
+  });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+     await queryInterface.dropTable("Books");
   }
 };
