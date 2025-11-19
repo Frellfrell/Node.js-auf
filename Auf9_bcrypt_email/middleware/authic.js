e6f38c68f4283c5ee602a8dbcee57d78d304185f
Frellfrell <ella.gubareva.24@gmail.com>
2025-11-19 18:06:16 +0100

    Update authic.js

--- Auf9_bcrypt_email/middleware/authic.js
+++ Auf9_bcrypt_email/middleware/authic.js
@@ -2,10 +2,13 @@ import User from '../models/user.js';
 //Проверка авторизации через userId в заголовке
 export const authMiddleware = async (req, res, next) => {
     const userId = req.headers['user-id'];
-  if (!userId) return res.status(401).json({ message: 'Нет авторизации' });
+  if (!userId)
+     return res.status(401).json({ message: 'Нет авторизации' });
 
-  const user = await User.findByPk(userId);
-  if (!user) return res.status(401).json({ message: 'Пользователь не найден' });
+  const user = await User.findOne({ where: { id: userId } });
+
+  if (!user)
+     return res.status(401).json({ message: 'Пользователь не найден' });
 
   req.user = user;
   next();
