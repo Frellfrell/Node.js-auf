5b59c9a6e9f91be920e5be680cf5157f982dd3d4
Frellfrell <ella.gubareva.24@gmail.com>
2025-11-19 18:14:16 +0100

    Update authic.js

--- Auf9_bcrypt_email/middleware/authic.js
+++ Auf9_bcrypt_email/middleware/authic.js
@@ -17,3 +17,19 @@ export const authMiddleware = async (req, res, next) => {
   req.user = user;
   next();
 };
+
+//Проверка mustChangePassword
+export const mustChangePasswordMiddleware = (req, res, next) => {
+  if (req.user.mustChangePassword) {
+    return res.status(403).json({ message: 'Необходимо сменить пароль' });
+  }
+  next();
+};
+
+// Проверка роли admin
+export const adminMiddleware = (req, res, next) => {
+  if (req.user.role !== 'admin') {
+    return res.status(403).json({ message: 'Доступ запрещен' });
+  }
+  next();
+};
