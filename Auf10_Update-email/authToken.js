9884f5335518149e453f69a65468b54af0ffe6c2
Frellfrell <ella.gubareva.24@gmail.com>
2025-11-24 15:06:25 +0100

    Update authToken.js

--- Auf10_Update-email/authToken.js
+++ Auf10_Update-email/authToken.js
@@ -7,7 +7,7 @@ async function getToken() {
             password: "12345",
         });
 
-        const token = response.data.token;
+        const token = response.data.TOKEN;
         console.log("Скопируй токен в  .env:");
         console.log(token);
     } catch (error) {
