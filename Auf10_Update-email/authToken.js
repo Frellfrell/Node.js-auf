f9bccf146fc102544293850b1f7ff3433a222af5
Frellfrell <ella.gubareva.24@gmail.com>
2025-11-24 13:46:02 +0100

    Created index.js and updet authTcken.js

---
+++ Auf10_Update-email/authToken.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+async function getToken() {
+    try {
+        const response = await axios.post("http://localhost:3000/login", {
+            email: "testuser@example.com",
+            password: "12345",
+        });
+
+        const token = response.data.token;
+        console.log("Скопируй токен в  .env:");
+        console.log(token);
+    } catch (error) {
+        console.error("Ошибка при получении токена:");
+        console.log(error.response?.data || error.message);
+    }
+}
+
+getToken();
