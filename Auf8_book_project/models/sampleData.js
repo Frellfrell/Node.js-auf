403976c3197900be8d73b69d739f96614811ec5d
Frellfrell <ella.gubareva.24@gmail.com>
2025-11-24 11:24:52 +0100

    Update sampleData.js

--- Auf8_book_project/models/sampleData.js
+++ Auf8_book_project/models/sampleData.js
@@ -1,5 +1,5 @@
 export const booksData = [
   { title: "Clean Code", author: "Robert Martin", year: 2008 },
   { title: "The Pragmatic Programmer", author: "Andrew Hunt", year: 1999 },
-  { title: "Eloquent JavaScript", author: "Marijn Haverbeke", year: 2018 }
+  { title: "Eloquent JavaScript", author: "Martin Haverbeke", year: 2018 }
 ];
