21ba95c5f86d147c39f48ff4b711aed97f2b368e
Frellfrell <ella.gubareva.24@gmail.com>
2025-11-24 13:53:08 +0100

    Update authenticateJWT.js

--- Auf10_Update-email/Middleware/authenticateJWT.js
+++ Auf10_Update-email/Middleware/authenticateJWT.js
@@ -1,31 +1,24 @@
 import jwt from "jsonwebtoken";
 
-const JWT_SECRET = process.env.JWT_SECRET || "supersecretkey";
+
 
 // middleware/authenticateJWT.js
 export default function authenticateJWT(req, res, next) {
+     try {
     const header = req.headers.authorization;
 
     if (!header) {
         return res.status(401).json({ error: "Authorization header missing" });
     }
 
-    const [type, token] = header.split(" ");
+     const token = authHeader.split(" ")[1];
+        if (!token) return res.status(401).json({ error: "Token not correct" });
 
-    if (type !== "Bearer" || !token) {
-        return res.status(401).json({ error: "Invalid Authorization format" });
-    }
-
-    try {
-        const payload = jwt.verify(token, JWT_SECRET);
-
-        if (!payload || !payload.id) {
-            return res.status(401).json({ error: "Invalid token payload" });
-        }
-
-        req.user = payload;
+        const decoded = jwt.verify(token, process.env.JWT_SECRET || "supersecretkey");
+        req.user = decoded;
         next();
-    } catch (err) {
-        return res.status(401).json({ error: "Token expired or invalid" });
+    } catch (error) {
+        console.error("Error JWT:", error.message);
+        return res.status(401).json({ error: "Token not correct" });
     }
 }
