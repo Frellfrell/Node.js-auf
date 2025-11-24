89e22f7bb67ebc9aa958137ca659f9ebfa1b0cd3
Frellfrell <ella.gubareva.24@gmail.com>
2025-11-24 14:07:36 +0100

    Update authenticateJWT.js

--- Auf10_Update-email/Middleware/authenticateJWT.js
+++ Auf10_Update-email/Middleware/authenticateJWT.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-
+const JWT_SECRET = process.env.JWT_SECRET || "supersecretkey";
 
 // middleware/authenticateJWT.js
 export default function authenticateJWT(req, res, next) {
