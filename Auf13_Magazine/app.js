e6cd018d0730faebc766fb87e729968184aa3a39
Frellfrell <ella.gubareva.24@gmail.com>
2025-12-03 20:57:57 +0100

    Update app.js

--- Auf13_Magazine/app.js
+++ Auf13_Magazine/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import testRoutes from './routes/testRoutes.js';
+import testRoutes from './routes/test.js';
 
 dotenv.config();
 
