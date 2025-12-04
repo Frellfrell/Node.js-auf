fa06c4a4f4e8cdd254768006f0818f43b97f9411
Frellfrell <ella.gubareva.24@gmail.com>
2025-12-04 10:40:43 +0100

    Update app.js

--- Auf14_CatProducts/app.js
+++ Auf14_CatProducts/app.js
@@ -1,3 +1,37 @@
-import express from 'express';  // Импортируем Express
-import mongoose from 'mongoose';  // Импортируем Mongoose
-import dotenv from 'dotenv';  /
+import express from 'express';  
+import mongoose from 'mongoose'; 
+import dotenv from 'dotenv'; 
+import Category from './models/Category.js'; 
+import Product from './models/Product.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
+
+
+// Маршрут для добавления категорий
+app.post('/categories', async (req, res) => {
+    try {
+        const category = new Category({ name: req.body.name });
+        await category.save();
+        res.status(201).send(category);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
+
+// Маршрут для добавления продукции 
+
+
+
+
+  app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});  
