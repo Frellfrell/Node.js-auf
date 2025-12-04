356c6eaa521566e8396f7b8740deca8b2bf366b3
Frellfrell <ella.gubareva.24@gmail.com>
2025-12-04 11:33:15 +0100

    Update app.js

--- Auf15_ESocket/app.js
+++ Auf15_ESocket/app.js
@@ -10,8 +10,29 @@ const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
 
+// Serve static files from the "public" directory
+app.use(express.static('public'));
 
+//Когда клиент подключается
 
+io.on('connection', (socket) => {
+    console.log('Новый пользователь подключен');
+
+    // Обработка сообщения от клиента
+    socket.on('chat message', (msg) => {
+        console.log('Сообщение от клиента: ' + msg);
+
+        // Отправка подтверждения клиенту
+        socket.emit('message received', 'Сообщение получено!');
+    });
+
+    // Когда клиент отключается
+    socket.on('disconnect', () => {
+        console.log('Пользователь отключен');
+    });
+});
+
+// Запуск сервера на порту 3000
 
 
 server.listen(PORT, () => {
