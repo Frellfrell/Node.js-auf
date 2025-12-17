1c2aed99ae4591d87ef0a20cfeeb675cc9aa62ea
Frellfrell <ella.gubareva.24@gmail.com>
2025-12-17 17:37:50 +0100

    Update User.js

--- Auf_finance-tracker/models/User.js
+++ Auf_finance-tracker/models/User.js
@@ -4,32 +4,32 @@ import mongoose from "mongoose";
 const transactionSchema = new mongoose.Schema({
     type: {
         type: String,
-        enum: ['income', 'expense'],
+        enum: ['income', 'expense'],//тип транзакции доход и расход
         required: true
     },
     amount: {
         type: Number,
-        required: true
+        required: true//сумма по умолчанию
     },
     date: {
         type: Date,
-        default: Date.now
+        default: Date.now //Дата текущая по умолчанию
     }
 });
 
 const userSchema = new mongoose.Schema({
      initialBalance: {
     type: Number,
-    required: [true, 'Начальный баланс обязателен'],
+    required: [true, 'Начальный баланс обязателен'], //Начальный баланс должен быть указан
     min: [0, 'Начальный баланс не может быть отрицательным']
   },
   currentBalance: {
     type: Number,
-    required: true,
+    required: true,//Текущий баланс должен быть указан
     min: 0
   },
   transactions: {
-    type: [transactionSchema],
+    type: [transactionSchema], //Массив транзакций
     default: []
   }
 });
