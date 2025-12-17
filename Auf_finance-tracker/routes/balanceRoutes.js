ff6cda32f221c724177f6736a90573d3e6c85e78
Frellfrell <ella.gubareva.24@gmail.com>
2025-12-17 18:05:22 +0100

    Update balanceRoutes.js

--- Auf_finance-tracker/routes/balanceRoutes.js
+++ Auf_finance-tracker/routes/balanceRoutes.js
@@ -38,3 +38,34 @@ router.post('/set-balance', async (req, res) => {
   }
 });
 
+//Маршрут для пополнения баланса пользователя
+router.post('/add-balance', async (req, res) => {
+  try {
+    const { userId, amount } = req.body;
+
+    // Проверка корректности суммы
+    if (!amount || typeof amount !== 'number' || amount <= 0) {
+      return res.status(400).json({ message: 'Сумма должна быть положительным числом' });
+    }
+
+    // Поиск пользователя по ID
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).json({ message: 'Пользователь не найден' });
+
+    // Обновление баланса и добавление транзакции
+    user.currentBalance += amount;
+    user.transactions.push({
+         type: 'income',
+          amount,
+        date: new Date()
+    });
+
+    // Сохранение изменений
+    await user.save();
+
+    // Ответ с подтверждением и обновленными данными пользователя
+    res.json({ message: 'Баланс пополнен', user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
