22fd35807d88b57bd88bde3c6293b75ddd26113c
Frellfrell <ella.gubareva.24@gmail.com>
2025-12-18 18:04:56 +0100

    Update app.js

--- Auf_finance-tracker/app.js
+++ Auf_finance-tracker/app.js
@@ -1,4 +1,7 @@
 import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./db/index.js";
+import balanceRoutes from "./routes/balanceRoutes.js";
 
 dotenv.config();
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/projectFin-tr";
