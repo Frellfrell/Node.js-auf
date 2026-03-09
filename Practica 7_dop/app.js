2f0b636e0f856e89436265b8a52e36aadb2ae003
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-09 22:42:00 +0100

    Update app.js

--- Practica 7_dop/app.js
+++ Practica 7_dop/app.js
@@ -17,3 +17,12 @@ async function startServer() {
   try {
     await sequelize.authenticate();
     console.log("Database connected");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
+
+startServer();
