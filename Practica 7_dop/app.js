1d07827aa9ebe8c19b13480babb21ac1b5c158e3
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-09 22:39:53 +0100

    Update app.js

---
+++ Practica 7_dop/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+import dotenv from "dotenv";
+import sequelize from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
