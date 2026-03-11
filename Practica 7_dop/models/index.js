44a612430fa6d809e3f461548295d88b810a260c
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-11 11:10:24 +0100

    Update index.js

--- Practica 7_dop/models/index.js
+++ Practica 7_dop/models/index.js
@@ -1,50 +1,41 @@
 "use strict";
 
-const fs = require("fs");
-const path = require("path");
-const Sequelize = require("sequelize");
-const process = require("process");
+import fs from "fs";
+import path from "path";
+import Sequelize from "sequelize";
+import sequelize from "../db"; // Импортируем sequelize из db.js
+
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
+const env = process.env.NODE_ENV || "development"; // Среда разработки
+const config = require(__dirname + "/../config/config.json")[env]; // Конфигурация из config.json
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config,
-  );
-}
+let sequelizeInstance = sequelize;
 
-fs.readdirSync(__dirname)
+fs.readdirSync(__dirname) // Читаем все файлы в текущей директории
   .filter((file) => {
     return (
-      file.indexOf(".") !== 0 &&
-      file !== basename &&
-      file.slice(-3) === ".js" &&
-      file.indexOf(".test.js") === -1
+      file.indexOf(".") !== 0 && // Исключаем скрытые файлы
+      file !== basename && // Исключаем сам index.js
+      file.slice(-3) === ".js" && // Берем только файлы с расширением .js
+      file.indexOf(".test.js") === -1 // Исключаем тестовые файлы
     );
   })
   .forEach((file) => {
     const model = require(path.join(__dirname, file))(
-      sequelize,
+      sequelizeInstance,
       Sequelize.DataTypes,
-    );
+    ); // Загружаем модели
     db[model.name] = model;
   });
 
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
-    db[modelName].associate(db);
+    db[modelName].associate(db); // Связи между моделями
   }
 });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.sequelize = sequelizeInstance; // Экспортируем sequelize
+db.Sequelize = Sequelize; // Экспортируем Sequelize
 
-module.exports = db;
+module.exports = db; // Экспортируем все модели
