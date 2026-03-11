bc3a68d9e727c2d366c24d184fc92e3bb8081964
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-11 23:03:46 +0100

    Update product.js

--- Practica 8_dop sequelize-one-to-many/models/product.js
+++ Practica 8_dop sequelize-one-to-many/models/product.js
@@ -1,14 +1,24 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/db.js";
 
-
 const Product = sequelize.define("Product", {
   name: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
   },
 
   price: {
     type: DataTypes.FLOAT,
-    allowNull: false
+    allowNull: false,
   },
+
+  categoryName: {
+    type: DataTypes.STRING,
+    references: {
+      model: "Categories",
+      key: "name",
+    },
+  },
+});
+
+export default Product;
