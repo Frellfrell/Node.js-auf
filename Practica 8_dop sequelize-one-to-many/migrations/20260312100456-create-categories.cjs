85a63f6884b28b1d42748400cf6b422c913c02c6
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-12 11:18:06 +0100

    Update 20260312100456-create-categories.cjs

--- Practica 8_dop sequelize-one-to-many/migrations/20260312100456-create-categories.cjs
+++ Practica 8_dop sequelize-one-to-many/migrations/20260312100456-create-categories.cjs
@@ -1,22 +1,27 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Categories", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE,
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Categories");
+  },
 };
