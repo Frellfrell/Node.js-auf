a732f68439dc6d05171e6f880ee605ec44ea2c61
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-12 11:31:04 +0100

    Update one-to-many-script.js

--- Practica 8_dop sequelize-one-to-many/one-to-many-script.js
+++ Practica 8_dop sequelize-one-to-many/one-to-many-script.js
@@ -1,1 +1,40 @@
 import { sequelize, Category, Product } from "./models/index.js";
+
+async function testOneToMany() {
+  try {
+    await sequelize.authenticate();
+    console.log("DB connected");
+
+    await sequelize.sync({ force: true });
+
+    const category = await Category.create({
+      name: "Cosmetics",
+    });
+
+    await Product.create({
+      name: "iPhone",
+      price: 1200,
+      categoryName: "Cosmetics",
+    });
+
+    await Product.create({
+      name: "Laptop",
+      price: 2000,
+      categoryName: "Cosmetics",
+    });
+
+    const result = await Category.findOne({
+      where: { name: "Cosmetics" },
+      include: Product,
+    });
+
+    console.log(JSON.stringify(result, null, 2));
+  } catch (error) {
+    console.error("Error:", error);
+  } finally {
+    await sequelize.close();
+    console.log("Connection closed");
+  }
+}
+
+testOneToMany();
