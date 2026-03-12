5d8ff672f7da29034b0f9d3e904a0515931b91fb
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-12 11:20:03 +0100

    Update 20260312100513-create-products.cjs

--- Practica 8_dop sequelize-one-to-many/migrations/20260312100513-create-products.cjs
+++ Practica 8_dop sequelize-one-to-many/migrations/20260312100513-create-products.cjs
@@ -3,13 +3,37 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
+   await queryInterface.createTable("Products", {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+
+    price: {
+      type: Sequelize.FLOAT,
+      allowNull: false
+    },
+
+    categoryName: {
+      type: Sequelize.STRING,
+      references: {
+        model: "Categories",
+        key: "name"
+      },
+      onUpdate: "CASCADE",
+      onDelete: "SET NULL"
+    },
+
+    createdAt: Sequelize.DATE,
+    updatedAt: Sequelize.DATE
+  });
+}
 
   async down (queryInterface, Sequelize) {
     /**
