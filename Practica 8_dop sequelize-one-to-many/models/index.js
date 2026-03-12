43919e9bfb710fd0d3b13ecf524fbdfacd252c03
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-12 10:57:30 +0100

    Update index.js

--- Practica 8_dop sequelize-one-to-many/models/index.js
+++ Practica 8_dop sequelize-one-to-many/models/index.js
@@ -1,1 +1,16 @@
+import sequelize from "../config/db.js";
 
+import Category from "./category.js";
+import Product from "./product.js";
+
+Category.hasMany(Product, {
+  foreignKey: "categoryName",
+  sourceKey: "name",
+});
+
+Product.belongsTo(Category, {
+  foreignKey: "categoryName",
+  targetKey: "name",
+});
+
+export { sequelize, Category, Product };
