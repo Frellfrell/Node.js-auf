64c12a58f8ea866ce831615ef636b4181358b9f4
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-12 11:20:50 +0100

    Update 20260312100513-create-products.cjs

--- Practica 8_dop sequelize-one-to-many/migrations/20260312100513-create-products.cjs
+++ Practica 8_dop sequelize-one-to-many/migrations/20260312100513-create-products.cjs
@@ -1,46 +1,41 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-   await queryInterface.createTable("Products", {
-    id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-
-    name: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-
-    price: {
-      type: Sequelize.FLOAT,
-      allowNull: false
-    },
-
-    categoryName: {
-      type: Sequelize.STRING,
-      references: {
-        model: "Categories",
-        key: "name"
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Products", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
       },
-      onUpdate: "CASCADE",
-      onDelete: "SET NULL"
-    },
 
-    createdAt: Sequelize.DATE,
-    updatedAt: Sequelize.DATE
-  });
-}
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+      price: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+
+      categoryName: {
+        type: Sequelize.STRING,
+        references: {
+          model: "Categories",
+          key: "name",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL",
+      },
+
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Products");
+  },
 };
