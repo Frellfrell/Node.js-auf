07adf67f5a88f40331ebf197ec268c7b9a8d6d83
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-19 22:33:48 +0100

    Update server.js

--- Practica 12_MyNotesApp/backend/server.js
+++ Practica 12_MyNotesApp/backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import todosRouter from "./routes/todos.js";
+import todosRouter from "./routes/todoRoutes.js";
 import mongoose from "mongoose";
 
 dotenv.config();
