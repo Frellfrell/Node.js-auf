4836d8a5243521507bd3f2058d7489c5000d0f8c
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-19 18:30:26 +0100

    Update NoteList.jsx

--- Practica 12_MyNotesApp/frontend/src/components/Notelist/NoteList.jsx
+++ Practica 12_MyNotesApp/frontend/src/components/Notelist/NoteList.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import NoteItem from "../NoteItem/NoteItem";
+import { fetchTodos } from "../../redux/App.jsx";
 import styles from "./NoteList.module.css";
+import { useEffect } from "react";
 
 function NoteList() {
   const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, [dispatch]);
 
   if (todos.length === 0) {
     return <p className={styles.empty}>NO Notes Available</p>;
@@ -13,7 +20,7 @@ function NoteList() {
   return (
     <div className={styles.list}>
       {todos.map((todo) => (
-        <NoteItem key={todo.id} todo={todo} />
+        <NoteItem key={todo._id} todo={todo} />
       ))}
     </div>
   );
