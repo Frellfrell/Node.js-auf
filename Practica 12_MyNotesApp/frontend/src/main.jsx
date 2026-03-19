88d63211bd26f5d414b50c9b093e7a0cc2be9a63
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-19 17:15:21 +0100

    Update main.jsx

--- Practica 12_MyNotesApp/frontend/src/main.jsx
+++ Practica 12_MyNotesApp/frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import NotesApp from "./NotesApp";
-import "./App.module.css";
 import { store } from "./redux/App.jsx";
 import { Provider } from "react-redux";
 
