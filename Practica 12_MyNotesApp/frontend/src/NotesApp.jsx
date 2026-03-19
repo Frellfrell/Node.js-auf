450027b535b27bcd956be611b95fa8812fc0c75e
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-19 17:00:09 +0100

    Update NotesApp.jsx

---
+++ Practica 12_MyNotesApp/frontend/src/NotesApp.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Footer from "./components/Footer/Footer";
+import NoteForm from "./components/NoteForm/NoteForm";
+import NoteList from "./components/NoteList/NoteList";
+import styles from "./NotesApp.module.css";
+
+const NotesApp = () => {
+  return (
+    <div className={styles.container}>
+      <Header />
+      <Main>
+        <div className={styles.notesCard}>
+          <NoteForm />
+          <NoteList />
+        </div>
+      </Main>
+      <Footer />
+    </div>
+  );
+};
+
+export default NotesApp;
