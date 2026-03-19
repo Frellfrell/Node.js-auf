dc9c29280b784659adcad264c018c28b2e4085b0
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-19 22:02:55 +0100

    Update NoteList.jsx

--- Practica 12_MyNotesApp/frontend/src/components/Notelist/NoteList.jsx
+++ Practica 12_MyNotesApp/frontend/src/components/Notelist/NoteList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import NoteItem from "../NoteItem/NoteItem";
-import { fetchTodos } from "../../redux/App.jsx";
+import { fetchTodos } from "../../redux/App.js";
 import styles from "./NoteList.module.css";
 import { useEffect } from "react";
 
