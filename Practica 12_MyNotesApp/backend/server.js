2f9567773260e4a744c7712d7f49474dcb6debdb
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-19 15:10:03 +0100

    Update server.js

--- Practica 12_MyNotesApp/backend/server.js
+++ Practica 12_MyNotesApp/backend/server.js
@@ -12,8 +12,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello from backend!");
-});
+app.use("/api/todos", todosRouter);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
