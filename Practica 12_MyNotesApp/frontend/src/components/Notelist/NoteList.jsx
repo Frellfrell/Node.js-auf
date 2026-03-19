99fd40796d2d376f44f10b8c51aa42cb933a056f
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-19 17:29:15 +0100

    Update NoteList.jsx

--- Practica 12_MyNotesApp/frontend/src/components/Notelist/NoteList.jsx
+++ Practica 12_MyNotesApp/frontend/src/components/Notelist/NoteList.jsx
@@ -4,16 +4,16 @@ import NoteItem from "../NoteItem/NoteItem";
 import styles from "./NoteList.module.css";
 
 function NoteList() {
-  const notes = useSelector((state) => state.notes);
+  const todos = useSelector((state) => state.todos);
 
-  if (notes.length === 0) {
+  if (todos.length === 0) {
     return <p className={styles.empty}>NO Notes Available</p>;
   }
 
   return (
     <div className={styles.list}>
-      {notes.map((note) => (
-        <NoteItem key={note.id} note={note} />
+      {todos.map((todo) => (
+        <NoteItem key={todo.id} todo={todo} />
       ))}
     </div>
   );
