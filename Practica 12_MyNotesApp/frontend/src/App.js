99929dcacad1c053f5163936266e4d4e1353f184
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-19 13:34:16 +0100

    delete frontend

--- Practica 12_MyNotesApp/frontend/src/App.js
+++ Practica 12_MyNotesApp/frontend/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Footer from "./components/Footer/Footer";
+import NoteForm from "./components/NoteForm/NoteForm";
+import NoteList from "./components/NoteList/NoteList";
+import styles from "./NotesApp.module.css";
 
-function App() {
+const NotesApp = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <Header />
+      <Main>
+        <div className={styles.notesCard}>
+          <NoteForm />
+          <NoteList />
+        </div>
+      </Main>
+      <Footer />
     </div>
   );
-}
+};
 
-export default App;
+export default NotesApp;
