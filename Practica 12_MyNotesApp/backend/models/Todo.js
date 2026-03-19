2b0aefa766aa0b9cbf2b109280e3efd400940aca
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-19 18:06:20 +0100

    Update Todo.js

--- Practica 12_MyNotesApp/backend/models/Todo.js
+++ Practica 12_MyNotesApp/backend/models/Todo.js
@@ -1,10 +1,13 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-const todoSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  text: { type: String, required: true },
-  complete: { type: Boolean, default: false },
-});
+const todoSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    text: { type: String, required: true },
+    complete: { type: Boolean, default: false },
+  },
+  { timestamps: true },
+);
 
 const Todo = mongoose.model("Todo", todoSchema);
 
