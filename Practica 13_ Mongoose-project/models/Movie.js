de3ea8acf537de799aaaf0814506995f92bedb3c
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-20 17:24:06 +0100

    Update Movie.js

--- Practica 13_ Mongoose-project/models/Movie.js
+++ Practica 13_ Mongoose-project/models/Movie.js
@@ -1,1 +1,7 @@
 import mongoose from "mongoose";
+
+const movieSchema = new mongoose.Schema({
+  title: String,
+  releaseYear: Number,
+  actors: [{ type: mongoose.Schema.Types.ObjectId, ref: "Actor" }],
+});
