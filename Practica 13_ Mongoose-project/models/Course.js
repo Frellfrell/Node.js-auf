728285d4bf8d6bca14391ae7f39a3ab74fa1caf8
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-20 17:19:09 +0100

    Update Course.js

--- Practica 13_ Mongoose-project/models/Course.js
+++ Practica 13_ Mongoose-project/models/Course.js
@@ -1,1 +1,7 @@
 import mongoose from "mongoose";
+
+const courseSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  students: [{ type: mongoose.Schema.Types.ObjectId, ref: "Student" }],
+});
