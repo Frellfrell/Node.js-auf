c9b339a57a77c5130becffa4333ed1cde32d12fb
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-20 17:15:02 +0100

    Update Product.js

--- Practica 13_ Mongoose-project/models/Product.js
+++ Practica 13_ Mongoose-project/models/Product.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
-
 const productSchema = new mongoose.Schema({
   name: String,
   price: Number,
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+  },
+});
