4a4afa1717688a75d3396c473df7ae6d58dfe5d6
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-20 17:27:07 +0100

    Update User.js

--- Practica 13_ Mongoose-project/models/User.js
+++ Practica 13_ Mongoose-project/models/User.js
@@ -1,1 +1,11 @@
 import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+    maxlength: 30,
+  },
