9e62736cc377daa71b7fe27818b9343ae1ce8a53
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-20 17:13:16 +0100

    Update Category.js

--- Practica 13_ Mongoose-project/models/Category.js
+++ Practica 13_ Mongoose-project/models/Category.js
@@ -3,3 +3,7 @@ import mongoose from "mongoose";
 const categorySchema = new mongoose.Schema({
   name: { type: String, required: true },
 });
+
+const Category = mongoose.model("Category", categorySchema);
+
+export default Category;
