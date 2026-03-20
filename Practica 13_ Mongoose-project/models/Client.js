56799b05b8e96df9e20266924f88bab34b3126d0
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-20 18:15:16 +0100

    Update Client.js

--- Practica 13_ Mongoose-project/models/Client.js
+++ Practica 13_ Mongoose-project/models/Client.js
@@ -6,3 +6,25 @@ const clientSchema = new mongoose.Schema({
     required: true,
     minlength: 2,
   },
+   contactPerson: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  phone: {
+    type: String,
+    required: true,
+    match: /^\+\d{1}\s\(\d{3}\)\s\d{3}-\d{4}$/,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^\S+@\S+\.\S+$/,
+  },
+  address: {
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    zipCode: { type: String, required: true },
+    country: { type: String, required: true },
+  },
