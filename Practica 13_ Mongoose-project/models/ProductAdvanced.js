44d0b87f9dcf881a5ba6283248b63e4432c17040
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-20 18:27:17 +0100

    Update ProductAdvanced.js

--- Practica 13_ Mongoose-project/models/ProductAdvanced.js
+++ Practica 13_ Mongoose-project/models/ProductAdvanced.js
@@ -6,3 +6,14 @@ const productSchema = new mongoose.Schema({
     required: true,
     minlength: 3,
   },
+   sku: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^[A-Z]{3}\d{5}$/,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0.01,
+  },
