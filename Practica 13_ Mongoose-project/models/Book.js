343c06c8059dc32712d006365f670fcb210c8a22
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-20 17:11:29 +0100

    Update Book.js

--- Practica 13_ Mongoose-project/models/Book.js
+++ Practica 13_ Mongoose-project/models/Book.js
@@ -1,7 +1,14 @@
 import mongoose from "mongoose";
 
-
 const bookSchema = new mongoose.Schema({
   title: { type: String, required: true },
   publishedDate: Date,
   author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Author",
+    required: true,
+  },
+});
+const Book = mongoose.model("Book", bookSchema);
+
+export default Book;
