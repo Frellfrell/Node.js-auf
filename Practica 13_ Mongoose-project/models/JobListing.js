256677b363fb41b72ab26a66f38bcb359e9625dc
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-20 17:47:36 +0100

    Update JobListing.js

--- Practica 13_ Mongoose-project/models/JobListing.js
+++ Practica 13_ Mongoose-project/models/JobListing.js
@@ -12,3 +12,11 @@ const jobListingSchema = new mongoose.Schema({
     required: true,
     minlength: 50,
   },
+  requirements: {
+    type: [String],
+    required: true,
+    validate: [
+      arr => arr.length >= 1 && arr.length <= 10,
+      "От 1 до 10 требований",
+    ],
+  },
