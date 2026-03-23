98b742887e13914dfce3bcca4998380a12fb9569
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-23 20:08:15 +0100

    Update auth.js

--- Practica_Project_routerAuth/routes/auth.js
+++ Practica_Project_routerAuth/routes/auth.js
@@ -8,32 +8,34 @@ const authRouter = Router();
 
 authRouter.post("/register", async (_, res) => {
   const db = getDb();
-  const { email, password } = req.body;
-  const existingUser = db.collection("users").findOne({ email });
+  const { username, password } = req.body;
+  const existingUser = db.collection("users").findOne({ username });
   if (existingUser) {
-    return res.status(400).json({ message: "Email already in use" });
+    return res.status(400).json({ message: "Username already in use" });
   }
 
   const hashedPassword = bcrypt.hashSync(password, 10);
-  await db.collection("users").insertOne({ email, password: hashedPassword });
+  await db
+    .collection("users")
+    .insertOne({ username, password: hashedPassword });
   // Registration logic here
   res.status(201).json({ message: "User registered successfully" });
 });
 
 authRouter.post("/login", async (_, res) => {
   const db = getDb();
-  const { email, password } = req.body;
+  const { username, password } = req.body;
 
-  const user = await db.collection("users").findOne({ email });
+  const user = await db.collection("users").findOne({ username });
   if (!user)
-    return res.status(400).json({ message: "Invalid email or password" });
+    return res.status(400).json({ message: "Invalid username or password" });
 
   const isPasswordValid = bcrypt.compareSync(password, user.password);
   if (!isPasswordValid)
-    return res.status(400).json({ message: "Invalid email or password" });
+    return res.status(400).json({ message: "Invalid username or password" });
 
   const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-    expiresIn: "1h",
+    expiresIn: "2h",
   });
   res.status(200).json({ token });
 });
