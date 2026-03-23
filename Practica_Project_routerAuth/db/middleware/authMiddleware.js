55474bce32287c7c0b6b47c2c568ca80e954fb19
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-23 20:05:38 +0100

    Update authMiddleware.js

--- Practica_Project_routerAuth/db/middleware/authMiddleware.js
+++ Practica_Project_routerAuth/db/middleware/authMiddleware.js
@@ -1,18 +1,19 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 export const authMiddleware = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-    if (!token) {
-        return res.status(401).json({ message: 'No token provided' });
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" });
+  }
 
-        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(403).json({ message: 'Invalid token' });
-        }
-        req.user = decoded;
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
+    req.user = decoded;
 
-        next();
-    });
+    next();
+  });
 };
