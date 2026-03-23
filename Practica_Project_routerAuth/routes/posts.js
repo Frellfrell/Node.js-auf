ea22978faadc760647ac13f44f257cbb729101bd
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-23 20:27:28 +0100

    Update posts.js
    receipt posts

--- Practica_Project_routerAuth/routes/posts.js
+++ Practica_Project_routerAuth/routes/posts.js
@@ -3,3 +3,10 @@ import { getDb } from "../db/index.js";
 import { authMiddleware } from "../db/middleware/authMiddleware.js";
 
 const postsRouter = Router();
+
+// Получение всех постов
+postsRouter.get("/", async (_, res) => {
+  const db = getDb();
+  const posts = await db.collection("posts").find().toArray();
+  res.json(posts);
+});
