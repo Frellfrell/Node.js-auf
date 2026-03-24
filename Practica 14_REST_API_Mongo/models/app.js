18c82eea1dd163588453c3b773903693d7c563a5
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-24 21:28:13 +0100

    Update app.js
    categories enquiry

--- Practica 14_REST_API_Mongo/models/app.js
+++ Practica 14_REST_API_Mongo/models/app.js
@@ -13,3 +13,13 @@ mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error("Connection Error:", err));
+
+app.post("/categories", async (req, res) => {
+  try {
+    const category = new Category(req.body);
+    await category.save();
+    res.status(201).json(category);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
