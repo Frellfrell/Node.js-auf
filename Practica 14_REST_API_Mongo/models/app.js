4b32128194d67798bdb303494dd24c27876a1881
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-24 21:21:09 +0100

    Update app.js

---
+++ Practica 14_REST_API_Mongo/models/app.js
@@ -0,0 +1,7 @@
+import express from "express";
+import mongoose from "mongoose";
+import Product from "./models/Product.js";
+import Category from "./models/Category.js";
+import dotenv from "dotenv";
+
+dotenv.config();
