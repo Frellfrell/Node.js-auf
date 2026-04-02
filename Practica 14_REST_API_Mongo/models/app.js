1ac3ee484709f44445a5b111cd713b583ed7258f
Frellfrell <ella.gubareva.24@gmail.com>
2026-04-02 20:22:12 +0200

    Update app.js

--- Practica 14_REST_API_Mongo/models/app.js
+++ Practica 14_REST_API_Mongo/models/app.js
@@ -80,11 +80,16 @@ app.get("/products/:id", async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
-  // PUT /products/:id (Обновление)
-app.put('/products/:id', async (req, res) => {
-
+// PUT /products/:id (Обновление)
+app.put("/products/:id", async (req, res) => {
   try {
-        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { 
-            new: true, 
-            runValidators: true 
-        });
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.json(product);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
