67a56a0faa4a0c07c0bd132ff7ea2fac09ed6e45
Frellfrell <ella.gubareva.24@gmail.com>
2026-04-02 20:20:05 +0200

    Update app.js

--- Practica 14_REST_API_Mongo/models/app.js
+++ Practica 14_REST_API_Mongo/models/app.js
@@ -70,10 +70,13 @@ app.get("/products", async (req, res) => {
   }
 });
 
-
 //Получение одного продукта с деталями категории
-app.get('/products/:id', async (req, res) => {
-    try {
-        const product = await Product.findById(req.params.id).populate('category');
-        if (!product) return res.status(404).json({ error: "Product not found" });
-        res.json(product);
+app.get("/products/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id).populate("category");
+    if (!product) return res.status(404).json({ error: "Product not found" });
+    res.json(product);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
