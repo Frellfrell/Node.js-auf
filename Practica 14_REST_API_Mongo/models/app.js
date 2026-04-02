73f78da0639ead3797ce2649327919e75907f4df
Frellfrell <ella.gubareva.24@gmail.com>
2026-04-02 20:23:24 +0200

    Update app.js

--- Practica 14_REST_API_Mongo/models/app.js
+++ Practica 14_REST_API_Mongo/models/app.js
@@ -95,5 +95,12 @@ app.put("/products/:id", async (req, res) => {
 });
 
 // DELETE /products/:id (Удаление)
-app.delete('/products/:id', async (req, res) => {
-    try {
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    if (!product) return res.status(404).json({ error: "Product not found" });
+    res.json({ message: "Product deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
