fbf2dc1d47d9471ead41c9951df1da5fd6d68551
Frellfrell <ella.gubareva.24@gmail.com>
2026-04-02 20:24:13 +0200

    Update app.js

--- Practica 14_REST_API_Mongo/models/app.js
+++ Practica 14_REST_API_Mongo/models/app.js
@@ -9,6 +9,8 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+const PORT = process.env.PORT || 3000;
+
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB Connected"))
